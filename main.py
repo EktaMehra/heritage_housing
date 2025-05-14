18cb5d9a49d03a9b7b4cda263d156b23db7a48c4
EktaMehra <mehra.ekta95@gmail.com>
2025-05-14 16:08:19 +0000

    Change file names for clarity

--- main.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Heritage Housing App", layout="wide")
-
-st.markdown("# Heritage Housing Dashboard")
-st.markdown("Use the sidebar to navigate through app features.")
